10db887247e48bacecddbaaa7f70f82443f6f6e3
Alex Palacios <78121995+alexpalaciosjr@users.noreply.github.com>
2022-05-06 13:15:22 -0400

    Basic skeleton for LSTM

---
+++ main_lstm.py
@@ -0,0 +1,22 @@
+import sys
+import csv
+import os
+import numpy as np
+import datetime
+import torch
+import torch.nn.functional as F
+
+
+class LSTM(torch.nn.Module):
+    ##TODO
+        
+
+if __name__ == "__main__":
+        
+    if MODE == "train":
+        ##TODO
+        
+        
+        
+    elif MODE == "predict":
+        ##TODO
